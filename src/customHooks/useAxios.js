a729325e7e26cd7f05df4d0e6ca89dc617104100
Stéphane Mestre <smestre@activeassurances.onmicrosoft.com>
2023-10-03 14:15:37 +0200

    pour faire étape 4

--- src/customHooks/useAxios.js
+++ src/customHooks/useAxios.js
@@ -4,18 +4,6 @@ import {useState, useEffect} from 'react'
 const useAxios= (url)=>
 {
 
- const [data, setData] = useState(null);
-
- useEffect(()=>{
-  axios.get(url).then(
-      rep=>{console.log(rep.data.Search);
-              setData(rep.data.Search);
-  }
-   );
-
- },[url])
-
-  return data;
 
 }
 
